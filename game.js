cb04877e7b70c3a9443bc1681606648e63e1ef44
Kristof Meyer <k.meyer.duisburg@gmail.com>
2020-01-14 02:18:06 +0100

    Version 1.4 [Scoreboard fixed]

--- game.js
+++ game.js
@@ -7,6 +7,7 @@ function item(x) {
     var userItem = x;
     var appItem = ["Rock", "Paper", "Scissor"][Math.floor(Math.random() * 3)];
 
+    /*     var compScore = []; */
     function compare() {
         if (appItem === userItem) {
             document.getElementById("actionMSG").innerHTML = "This is a DRAW";
@@ -18,6 +19,7 @@ function item(x) {
             if (userItem === 'Scissor') {
                 document.getElementById("actionMSG").innerHTML = "ROCK destroys SCISSOR";
                 document.getElementById("result").innerHTML = "You LOOSE!";
+
                 /* document.getElementById("userIndicator").innerHTML = "you chose scissor"; */
             }
         }
@@ -26,9 +28,11 @@ function item(x) {
             if (userItem === 'Rock') {
                 document.getElementById("actionMSG").innerHTML = "PAPER covers ROCK";
                 document.getElementById("result").innerHTML = "You LOOSE!";
+
             } else {
                 document.getElementById("actionMSG").innerHTML = "SCISSOR cuts PAPER";
                 document.getElementById("result").innerHTML = "You WIN!";
+                document.getElementById(userScore).innerHTML = userScore; //call function in here
             }
         }
         else if (appItem === 'Scissor') {
@@ -36,11 +40,47 @@ function item(x) {
             if (userItem === 'Rock') {
                 document.getElementById("actionMSG").innerHTML = "ROCK destroys SCISSOR";
                 document.getElementById("result").innerHTML = "You WIN!";
+
             } else if (userItem === 'Paper') {
                 document.getElementById("actionMSG").innerHTML = "SCISSOR cuts PAPER";
                 document.getElementById("result").innerHTML = "You LOOSE!";
+
             }
         }
     };
     compare();
-} 1
+
+}
+function updateScores(winner) {
+
+    if (winner == userItem) {
+        userScore = userScore + 1;
+        document.getElementById(userScore).innerHTML = userScore;
+    }
+
+    else if (winner == compItem) {
+        compScore = compScore + 1;
+        document.getElementById(compScore).innerHTML = compScore;
+    }
+};
+
+function updateScores() {
+    var compScore = [0];
+    var userScore = [0];
+    if (appItem === 'Rock') {
+        updateScores(comp);
+    }
+    else if (userItem === 'Scissor') {
+        userScore = userScore + 1;
+        document.getElementById("userScore").innerHTML = compScore;
+    }
+
+
+};
+
+function ubdateScore(user) {
+    document.getElementById("compScore").innerHTML = compScore + 1;
+}
+function ubdateScore(comp) {
+    document.getElementById("compScore").innerHTML = compScore + 1;
+}
